0f17d6f6441a9216d690ca471591cecf406a6b37
SWS-5007 <119567356+SWS-5007@users.noreply.github.com>
2023-08-30 08:40:18 -0400

    Removed the Unnecessary Codes

--- client/src/hook/GetUppercaseText.js
+++
@@ -1,5 +0,0 @@
-export const GetUppercaseText = (text) => {
-  let upperCasedText = text.charAt(0).toUpperCase() + text.slice(1);
-
-  return upperCasedText;
-};
--- client/src/hook/GoScrollTop.js
+++
@@ -1,5 +0,0 @@
-export const GoScrollTop = () => {
-  document.querySelector(".App").scrollTo({ top: 0 });
-
-  return true;
-};
